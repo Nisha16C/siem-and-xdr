37f2479791a2538deb78c37a44f86e2ebe591337
Ankit Dwivedi <ankit.dwivedi@os3.com>
2023-08-03 12:18:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,25 +12,25 @@
 ## create vms
 
 resource "cloudstack_instance" "vm1" {
-  name             = "frontend-vm"
+  name             = "soc"
   service_offering = "test-vm1"
   template         = "ub20.04-no-disk-raw-kvm-amd64.img"
   zone             = "WIPL-bz4"
   network_id       = "182f40ba-1b00-4729-bd4f-a7e9a2f33c11"
   security_group_ids = ["c3547800-2d43-454b-93df-59c1013f78e3"]
   keypair          = "rdp"
-  ip_address       = "192.168.30.70"
+  #ip_address       = "192.168.30.70"
   expunge          = "true"
 }
 
-resource "cloudstack_instance" "vm2" {
-  name             = "backend-vm"
-  service_offering = "test-vm1"
-  template         = "ub20.04-no-disk-raw-kvm-amd64.img"
-  zone             = "WIPL-bz4"
-  network_id       = "182f40ba-1b00-4729-bd4f-a7e9a2f33c11"
-  security_group_ids = ["c3547800-2d43-454b-93df-59c1013f78e3"]
-  keypair          = "rdp"
-  ip_address       = "192.168.30.71"
-  expunge          = "true"
-}
+// resource "cloudstack_instance" "vm2" {
+//   name             = "backend-vm"
+//   service_offering = "test-vm1"
+//   template         = "ub20.04-no-disk-raw-kvm-amd64.img"
+//   zone             = "WIPL-bz4"
+//   network_id       = "182f40ba-1b00-4729-bd4f-a7e9a2f33c11"
+//   security_group_ids = ["c3547800-2d43-454b-93df-59c1013f78e3"]
+//   keypair          = "rdp"
+//   ip_address       = "192.168.30.71"
+//   expunge          = "true"
+// }
