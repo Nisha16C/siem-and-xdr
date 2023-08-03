94ca088567019044c7226e2f356f4700c0e8e62e
Ankit Dwivedi <ankit.dwivedi@os3.com>
2023-08-03 09:28:30 +0000

    Add new file

---
+++ main.tf
@@ -0,0 +1,36 @@
+## assign static ip
+// resource "cloudstack_static_nat" "vm1_static_ip" {
+//   virtual_machine_id = cloudstack_instance.vm1.id
+//   ip_address         = "192.168.30.54"
+// }
+
+// resource "cloudstack_static_nat" "vm2_static_ip" {
+//   virtual_machine_id = cloudstack_instance.vm2.id
+//   ip_address         = "192.168.30.53"
+// }
+
+## create vms
+
+resource "cloudstack_instance" "vm1" {
+  name             = "frontend-vm"
+  service_offering = "test-vm1"
+  template         = "ub20.04-no-disk-raw-kvm-amd64.img"
+  zone             = "WIPL-bz4"
+  network_id       = "182f40ba-1b00-4729-bd4f-a7e9a2f33c11"
+  security_group_ids = ["c3547800-2d43-454b-93df-59c1013f78e3"]
+  keypair          = "rdp"
+  ip_address       = "192.168.30.53"
+  expunge          = "true"
+}
+
+resource "cloudstack_instance" "vm2" {
+  name             = "backend-vm"
+  service_offering = "test-vm1"
+  template         = "ub20.04-no-disk-raw-kvm-amd64.img"
+  zone             = "WIPL-bz4"
+  network_id       = "182f40ba-1b00-4729-bd4f-a7e9a2f33c11"
+  security_group_ids = ["c3547800-2d43-454b-93df-59c1013f78e3"]
+  keypair          = "rdp"
+  ip_address       = "192.168.30.54"
+  expunge          = "true"
+}
