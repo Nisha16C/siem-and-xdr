217908a8ba1938a7e35c9f0949c11405ed4fca44
Ankit Dwivedi <ankit.dwivedi@os3.com>
2023-08-09 10:31:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "cloudstack_instance" "vm1" {
     passwd -e ubuntu  # This line will disable password expiration
     sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
     systemctl restart sshd
+    touch /ankit.txt
     EOF
     // #USER DATA
     // user: ubuntu
