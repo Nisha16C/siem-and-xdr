1e94697f17f3a3a99ffa13229ce3837d5b259107
Ankit Dwivedi <ankit.dwivedi@os3.com>
2023-08-10 11:00:42 +0000

    Update install_tool.sh

--- install_tool.sh
+++ install_tool.sh
@@ -1,2 +1,3 @@
 touch /ankit.txt
 curl -sO https://packages.wazuh.com/4.4/wazuh-install.sh && sudo bash ./wazuh-install.sh -a |tee wazuh-install-log-creds.txt
+grep -E "User:|Password:" wazuh-install-log-creds.txt >> wazuh_credentials.txt
